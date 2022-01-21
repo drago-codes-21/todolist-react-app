8e3ce394bb957ac47ec522143e5da3f541eef498
Sartthhak <nalawade.sarthak21@gmail.com>
2022-01-21 16:38:12 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import TodoList from "./component/TodoList";
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="app-header">
+        <h1>TODO APP</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
